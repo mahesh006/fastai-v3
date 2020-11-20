a0ccc6f8ed33daa88a1b12e55e81c7c780366a11
Mahesh <55144460+mahesh006@users.noreply.github.com>
2020-11-20 23:34:29 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1-9VhQh-06SwLfqU8ReZEBUDOkwxkebkJ'
+export_file_url = 'https://www.dropbox.com/s/emgc6yuomqp985f/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['benign', 'malignant', 'mole']
